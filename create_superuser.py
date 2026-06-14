f4a3f3d6c1a238f20d5f49c633868872cf62d90e
Bahoque <bahoque.sena@gmail.com>
2026-06-13 19:36:05 -0500

    se corrigio el archivo

--- create_superuser.py
+++ create_superuser.py
@@ -7,6 +7,11 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username='GAVIRIA').exists():
-    User.objects.create_superuser('GAVIRIA', 'gaviria@gmail.com', '1234567')
-    print("Superuser creado")
+try:
+    if not User.objects.filter(username='GAVIRIA').exists():
+        User.objects.create_superuser('GAVIRIA', 'gaviria@gmail.com', '1234567')
+        print("Superuser creado")
+    else:
+        print("Superuser ya existe, omitiendo...")
+except Exception as e:
+    print(f"Error al crear superuser: {e}")

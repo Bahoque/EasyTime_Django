214d2bd63e07f6f56ebacde3e23efb7236067ffb
Bahoque <bahoque.sena@gmail.com>
2026-06-13 19:32:35 -0500

    se crea el superuser desde codigo para evitar errores en la base de datos en render

--- create_superuser.py
+++ create_superuser.py
@@ -1,12 +1,12 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easytime.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
+if not User.objects.filter(username='GAVIRIA').exists():
     User.objects.create_superuser('GAVIRIA', 'gaviria@gmail.com', '1234567')
     print("Superuser creado")
